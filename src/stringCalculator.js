dbcf8a0634a194edeadf1d7069e5df3deccddf72
NathiPrakash17 <manitamiltkv1997@gmail.com>
2024-08-01 10:19:36 +0530

    Update stringCalculator.js

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,1 +1,15 @@
+function add(numbers) {
+  if (numbers === '') {
+    return 0;
+  }
+  const numbersArray = numbers.split(',');
+  let sum = 0;
+  for (let i = 0; i < numbersArray.length; i++) {
+    sum += +numbersArray[i];
+  }
+  return sum;
+}
 
+module.exports = {
+    add
+};
