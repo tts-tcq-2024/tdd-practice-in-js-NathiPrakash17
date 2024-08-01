c4bb3cde5de651614cd6d2b8bed676c79383cbd4
NathiPrakash17 <manitamiltkv1997@gmail.com>
2024-08-01 11:11:31 +0530

    Update stringCalculator.js - custom delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,10 +1,21 @@
+function extractDelimiter(numbers) {
+  let delimiter = ',';
+  let numberStrings = numbers;
+
+  if (numbers.startsWith('//')) {
+    const delimiterEnd = numbers.indexOf('\n');
+    delimiter = numbers.substring(2, delimiterEnd);
+    numberStrings = numbers.substring(delimiterEnd + 1);
+  }
+  return numberStrings.split(delimiter);
+}
+
 function add(numbers) {
   if (numbers === '') {
     return 0;
   }
   
-const delimiterRegex = /,|\n/g;
-const numberStrings = numbers.split(delimiterRegex);
+const numberStrings = processDelimiter(numbers);
   
   let sum = 0;
   for (let i = 0; i < numberStrings.length; i++) {
