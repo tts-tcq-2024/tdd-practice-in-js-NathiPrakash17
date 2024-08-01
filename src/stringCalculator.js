53dc3c864660341b1913750532fc63245fecfd5f
NathiPrakash17 <manitamiltkv1997@gmail.com>
2024-08-01 11:51:23 +0530

    Update stringCalculator.js negatives

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -10,13 +10,26 @@ function processDelimiter(numbers) {
  return numberStrings.split(new RegExp(`[${delimiter}\n]`));
 }
 
+function checkForNegatives(numberStrings) {
+  const negatives = numberStrings
+    .map(number => Number(number))
+    .filter(number => number < 0);
+  if (negatives.length > 0) {
+    throw new Error(`Negatives not allowed: ${negatives.join(', ')}`);
+  }
+}
+
 function add(numbers) {
   if (numbers === '') {
     return 0;
   }
-  
+
+  // delimiters removal
 const numberStrings = processDelimiter(numbers);
-  
+  // negative check
+checkForNegatives(numberStrings);
+
+  // sum
   let sum = 0;
   for (let i = 0; i < numberStrings.length; i++) {
     sum += Number(numberStrings[i]);
